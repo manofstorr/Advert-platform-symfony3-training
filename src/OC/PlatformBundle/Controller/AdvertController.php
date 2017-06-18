ff31625918b53b104132104d10aee4be3c924dcc
David Baudry <david.baudry@zoho.com>
2017-06-18 10:08:03 +0200

    Basic routing system for the platform bundle (and a sample method)

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -14,6 +14,8 @@ namespace OC\PlatformBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
 class AdvertController extends Controller
 {
     public function indexAction()
@@ -29,10 +31,73 @@ class AdvertController extends Controller
          * de Symfony.
          */
         $content = $this->get('templating')
-            ->render('OCPlatformBundle:Advert:index.html.twig', array(
+            ->render('OCPlatformBundle:Advert:index.html.twig');
+        return new Response($content);
+    }
+
+    public function viewAction($id)
+    {
+        // todo : code
+        return new Response('coucou ' . $id);
+    }
+
+    // On récupère tous les paramètres en arguments de la méthode
+    public function viewSlugAction($slug, $year, $_format)
+    {
+        return new Response(
+            "On pourrait afficher l'annonce correspondant au
+            slug '" . $slug . "', créée en " . $year . " et au format " . $_format . "."
+        );
+    }
+
+
+    /* CRUD */
+
+    public function addAction()
+    {
+        // todo : code
+    }
+
+    
+    /* SAMPLES BELOW */
+
+    /*
+     * A sample to use git, eat after reading
+     */
+    public function goodbyeAction()
+    {
+
+
+        //short and long method
+        $url = $this->get('router')->generate('oc_platform_home');
+        $url = $this->generateUrl('oc_platform_home');
+
+        // sample use of generating absolute interface
+        $url = $this->get('router')->generate('oc_platform_home', array(),
+            UrlGeneratorInterface::ABSOLUTE_URL);
+
+
+        $advert_id = 5;
+
+        // sample use of generating url from router
+        $url = $this->get('router')->generate(
+            'oc_platform_view', // 1er argument : le nom de la route
+            array('id' => $advert_id)    // 2e argument : les valeurs des paramètres
+        );
+
+
+        // $url vaut « /platform/advert/5 »
+        $content = $this->get('templating')
+            ->render('OCPlatformBundle:Advert:goodbye.html.twig', array(
                 'nom' => 'David',
-                'chiffre' => '68',
+                'heure' => date('H:i'),
+                'url' => $url,
+                'advert_id' => $advert_id,
             ));
+
+
+
+
         return new Response($content);
     }
 }
